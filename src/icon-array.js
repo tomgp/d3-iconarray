e37718bfb2e0c47dd19a3159e7b09e99afd04fd7
tomgp <tom.g.pearson@gmail.com>
2016-02-16 17:48:15 +0000

    position and dimension functions

--- src/icon-array.js
+++ src/icon-array.js
@@ -6,12 +6,53 @@ export default function() {
 	function layout(data){
 		//work our missing height, width stuff
 		console.log('new stuff');
+		setDimensions(data.length);
+
 		return data.map(function(d,i){
 			return {
 				data:d,
-				position:position(i, width, height, widthFirst)
+				position:position(i)
 			};
 		});
+	}
+
+	function position(i){
+		if(isNaN(width) || isNaN(height)){ 
+			console.log('Warning: width/height undefined') 
+			return 0;
+		}
+		if(widthFirst){
+			return {
+				x: i % width,
+				y: Math.floor( i/width )
+			};
+		}else{
+			return {
+				x: Math.floor( i/height ),
+				y: i % height
+			};
+		}
+	}
+
+	function setDimensions(l){
+		//neither width or height is defined
+		if(isNaN(width) && isNaN(height)){
+			if(widthFirst){ 
+				width = Math.ceil( Math.sqrt(l) );
+				height = Math.ceil( l / width );
+			}else{
+				height = Math.ceil( Math.sqrt(l) );
+				width = Math.ceil( l / height );
+			}
+		}else if(isNaN(width)){	//width undefined
+			width = Math.ceil( l / height );
+		}else if(isNaN(height)){ //height undefined
+			height = Math.ceil( l / width );
+		}
+	}
+
+	layout.position = function(x){
+		return position(x);
 	}
 
 	layout.width = function(x){
@@ -34,6 +75,8 @@ export default function() {
 
 	return layout;
 };
+
+
 
 function position(item, width, height, widthFirst){
 	return i;
